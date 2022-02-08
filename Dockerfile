a50b6695e4f155450d40aaa2b9ffa696d2309f74
Alex Bakoushin <alex@bakoush.in>
2022-02-08 12:10:34 +0300

    Fork core UI code from monorepo

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:16-alpine
+ENV NPM_CONFIG_LOGLEVEL=warn
+WORKDIR /usr/src/app
+COPY . .
+RUN npm install --only=production
+CMD npm docker
---
+++ config.js
@@ -0,0 +1,5 @@
+const port = process.env.PORT;
+const providerURL = process.env.PROVIDER_URL;
+const coreAddress = process.env.CORE_ADDRESS;
+
+export { port, providerURL, coreAddress };
